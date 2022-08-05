e845fc0f904bd218cc1c046f2884cf78d92ea397
Del Campo Fabian <fabydelcampo@hotmail.com>
2022-08-05 06:51:15 -0300

    Tienda deportiva

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 //Clase para crear los productos 
 class Producto {
-    constructor(nombre, imagen, precio, id) {
+    constructor(nombre, imagen, precio, categoria, id) {
         this.nombre = nombre;
         this.imagen = imagen;
         this.precio = precio;
+        this.categoria = categoria;
         this.id = id;
     }
 }
@@ -14,43 +15,58 @@ let carrito = []
 let listaDeProductos = []
 let total = 0
 let tarjetaContenedora
+let botonFiltroIndumentaria
+let botonFiltroAccesorios
+let botonFiltroSnacks
+let botonFiltroSuplementos
+let botonTodosProductos
 let contador = 0
 let contenedorContador
-let llamarApi
 let object
 let contenedorStorageNombre
 let contenedorStorageId
 let contenedorStoragePrecio
 
+
+
 //Creación de productos
-let indumentaria1 = new Producto("Short", "ropaMujer6.jpg", 1500, 1)
-let indumentaria2 = new Producto("Remera y corto", "ropaHombre2.jpg", 5300, 2)
-let indumentaria3 = new Producto("Camiseta Boca Juniors", "ropaHombre3.jpg", 6500, 3)
-let indumentaria4 = new Producto("Pantalon corto", "ropaHombre4.jpg", 1600, 4)
-let indumentaria5 = new Producto("Remera", "ropaHombre5.jpg", 2200, 5)
-let indumentaria6 = new Producto("Remera", "ropaHombre6.jpg", 2200, 6)
-let accesorios1 = new Producto("Mancuerna", "mancuerna4.jpg", 7500, 7)
-let accesorios2 = new Producto("Discos con agarre", "discos.jpg", 1500, 8)
-let accesorios3 = new Producto("Barra", "barras12.jpg", 1200, 9)
-let accesorios4 = new Producto("Mancuerna", "mancuernas3.jpg", 800, 10)
-let accesorios5 = new Producto("Discos", "discos8.jpg", 1000, 11)
-let accesorios6 = new Producto("Mancuernas", "barras11.jpg",1200, 12)
-let suplemento1 = new Producto("Proteina Star Nutrition", "suplementos5.jpg", 2500, 13)
-let suplemento2 = new Producto("Creatina Star Nutrition", "suplementos6.jpg", 1600, 14)
-let suplemento3 = new Producto("Proteina HardCore", "suplementos4.jpg", 1100, 15)
+let indumentaria1 = new Producto("Short", "ropaMujer6.jpg", 1500, "indumentaria", 1)
+let indumentaria2 = new Producto("Remera y corto", "ropaHombre2.jpg", 5300, "indumentaria", 2)
+let indumentaria3 = new Producto("Camiseta Boca Juniors", "ropaHombre3.jpg", 6500, "indumentaria", 3)
+let indumentaria4 = new Producto("Pantalon corto", "ropaHombre4.jpg", 1600, "indumentaria", 4)
+let indumentaria5 = new Producto("Remera", "ropaHombre5.jpg", 2200, "indumentaria", 5)
+let indumentaria6 = new Producto("Remera", "ropaHombre6.jpg", 2200, "indumentaria", 6)
+let accesorios1 = new Producto("Mancuerna", "mancuerna4.jpg", 7500, "accesorios", 7)
+let accesorios2 = new Producto("Discos con agarre", "discos.jpg", 1500, "accesorios", 8)
+let accesorios3 = new Producto("Barra", "barras12.jpg", 1200, "accesorios", 9)
+let accesorios4 = new Producto("Mancuerna", "mancuernas3.jpg", 800, "accesorios", 10)
+let accesorios5 = new Producto("Discos", "discos8.jpg", 1000, "accesorios", 11)
+let accesorios6 = new Producto("Mancuernas", "barras11.jpg", 1200, "accesorios", 12)
+let suplemento1 = new Producto("Proteina Star Nutrition", "suplementos5.jpg", 2500, "suplementos", 13)
+let suplemento2 = new Producto("Creatina Star Nutrition", "suplementos6.jpg", 1600, "suplementos", 14)
+let suplemento3 = new Producto("Proteina HardCore", "suplementos4.jpg", 1100, "suplementos", 15)
 
 //Push de productos
+
 listaDeProductos.push(indumentaria1, indumentaria2, indumentaria3, indumentaria4, indumentaria5, indumentaria6, )
 listaDeProductos.push(accesorios1, accesorios2, accesorios3, accesorios4, accesorios5, accesorios6, )
 listaDeProductos.push(suplemento1, suplemento2, suplemento3)
 
 
 //FUNCIONES
+
+console.log(listaDeProductos)
 function registrarHtml() {
     sumarCarrito = document.querySelector(".contador-productos")
     tarjetaContenedora = document.querySelector("#productos")
-    llamarApi = document.querySelector(".productos__verMas")
+    botonFiltroIndumentaria = document.querySelector("#indumentaria")
+    botonFiltroAccesorios = document.querySelector("#accesorios")
+    botonFiltroSnacks = document.querySelector("#snacks")
+    botonFiltroSuplementos = document.querySelector("#suplementos")
+    botonTodosProductos = document.querySelector("#verTodo")
+    
 }
+
 
 function registrarProductos() {
     sumarCarrito.innerText = `${cantidadProductos.length}`
@@ -71,12 +87,15 @@ function tarjetaDom(producto) {
         </div>`;
     return tarjetaDom
 }
+
+
 //Pintar las tarjetas de los productos
-function pintarCards(){
+function pintarCards() {
     listaDeProductos.forEach((producto) => {
         tarjetaContenedora.innerHTML += tarjetaDom(producto)
     })
 }
+
 
 function obtenerApi() {
     fetch("https://62e2df9db54fc209b881e38a.mockapi.io/productos")
@@ -84,18 +103,8 @@ function obtenerApi() {
         .then((data) => {
             listaDeProductos.push(...data)
         })
-}
+} 
 
-function pintarProductosApi(){
-    llamarApi.addEventListener("click",()=>{
-        let productosApi = listaDeProductos.filter((producto) => producto.id >= 16)
-        productosApi.forEach((producto) => {
-            tarjetaContenedora.innerHTML += tarjetaDom(producto)
-        })
-        generarCarrito()
-        llamarApi.classList.toggle("registro__invisible")
-    })
-}
 //Función cartel de compra
 function cartelProductoAgregado() {
     Toastify({
@@ -121,13 +130,13 @@ function obtenerStorage() {
     //Storage de cantidad de productos en el carrito
     let storageCantProductos = localStorage.getItem("contador")
     let numeroProductos = JSON.parse(storageCantProductos);
-    cantidadProductos = numeroProductos;
-    contenedorContador = cantidadProductos.length;
+    cantidadProductos = numeroProductos ? numeroProductos : [];
+    contenedorContador = cantidadProductos ? cantidadProductos.length : 0;
     sumarCarrito.innerText = `${contenedorContador}`
     //Storage de la lista de productos
     let storage = localStorage.getItem("carrito");
     object = JSON.parse(storage);
-    carrito = object
+    carrito = object ? object : []
     carrito.forEach((producto) => {
         contenedorStorageNombre = producto.nombre;
         contenedorStorageId = producto.id;
@@ -136,7 +145,7 @@ function obtenerStorage() {
 }
 
 //Generación de carrito
-function generarCarrito(){
+function generarCarrito() {
     let botones = document.querySelectorAll(".boton");
     let arrayBotones = Array.from(botones)
     arrayBotones.forEach(boton => {
@@ -159,6 +168,30 @@ function main() {
     generarCarrito()
     obtenerApi()
     obtenerStorage()
-    pintarProductosApi()
 }
 main()
+
+botonTodosProductos.addEventListener("click", () => {
+    tarjetaContenedora.innerHTML = ""
+    pintarCards()
+    generarCarrito()
+})
+
+
+function botonesFiltros (botones,categoria){
+    botones.addEventListener("click", () => {
+        let productosFiltrados = listaDeProductos.filter((producto) => producto.categoria === categoria)
+        console.log(productosFiltrados);
+        tarjetaContenedora.innerHTML = ""
+        productosFiltrados.forEach((producto) => {
+            tarjetaContenedora.innerHTML += tarjetaDom(producto)
+        })
+        generarCarrito()
+    })
+}
+ 
+botonesFiltros(botonFiltroSnacks, "snacks")
+botonesFiltros(botonFiltroIndumentaria,"indumentaria")
+botonesFiltros(botonFiltroAccesorios, "accesorios")
+botonesFiltros(botonFiltroSuplementos, "suplementos")
+
